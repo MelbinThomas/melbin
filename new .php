c0884c075e24a2bb2f0b87a4e43fdc387d2e8c11
MelbinThomas <43873216+MelbinThomas@users.noreply.github.com>
2019-02-19 05:24:08 +0530

    Add files via upload

---
+++ new .php
@@ -0,0 +1,37 @@
+<html>
+ <body background="L.jpeg">  
+  <h1 style="border:2px solid DodgerBlue;">FRIENDS CORNER</h1>
+  
+<style>
+
+.button {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+
+.button2 {background-color: #008CBA;} /* Blue */
+.button3 {background-color: #f44336;} /* Red */ 
+.button4 {background-color: #ee82ee; color: black;} /* Gray */ 
+.button5 {background-color: #ee82ee;} /* Black */
+</style>
+</head>
+<body>
+
+
+<button class="button">MANU JOSEPH</button><BR>
+<button class="button button2">MANUMON MATHEW   </button><BR>
+<button class="button button3">ALAN K ABRAHAM</button><BR>
+<button class="button button4">ARAVIND NAIR</button>
+
+
+
+</body>
+</html>
